5f778c420480625292fe5506d3b84caf546ddb21
Luis Alberto Fernández <luisalb@acarballeira.local>
2022-11-04 18:02:42 +0100

    día 4/11/2022 JuegoNumeros sesiones partidas jugadas

--- ejercicios/juegoNumeroSesiones/index.php
+++ ejercicios/juegoNumeroSesiones/index.php
@@ -1,9 +1,11 @@
 <?php
-
+define("CLAVE","asdfjn.-ms#rhj3DF");
+const CLAVE2 = "asdfjn.-ms#rhj3DF";
 session_start();
 $minimo = 1;
 $maximo = 100;
 $historico = [];
+$numPartidas = 0;
 
 function nuevaPartida(&$secreto,&$intentos,&$historico) {
     $secreto = random_int(1,100);
@@ -14,10 +16,11 @@ function nuevaPartida(&$secreto,&$intentos,&$historico) {
     $historico = $_SESSION["historico"];
 }
 
-function recuperarDatosPartida(&$secreto,&$intentos,&$historico) {
+function recuperarDatosPartida(&$secreto,&$intentos,&$historico,&$numPartidas) {
     $secreto = $_SESSION["secreto"];
     $intentos = $_SESSION["intentos"];
     $historico = $_SESSION["historico"];
+    RecuperarPartidasJugadas($numPartidas);
 }
 
 function actualizarIntentos(&$intentos) {
@@ -30,10 +33,30 @@ function guardarHistorico($numero) {
     return $_SESSION["historico"];
 }
 
+function ActualizarPartidasJugadas(&$numPartidas){
+    $numPartidas++;
+    setcookie("numPartidas",$numPartidas,time()+3600000);
+    setcookie("hash_numPartidas",hash("sha1",($numPartidas.CLAVE)),time()+3600000);
+}
+
+function RecuperarPartidasJugadas(&$numPartidas){
+    if (isset($_COOKIE["numPartidas"]) && isset($_COOKIE["hash_numPartidas"])) {
+        $numPartidas =  $_COOKIE["numPartidas"];
+        $hash= hash("sha1",($numPartidas.CLAVE));
+        if ($hash!=$_COOKIE["hash_numPartidas"]) {
+            $numPartidas = -1; 
+            ActualizarPartidasJugadas($numPartidas); 
+        }
+    } else {
+        $numPartidas = -1; 
+        ActualizarPartidasJugadas($numPartidas); 
+    }
+}
+
 if (!isset($_SESSION["secreto"]) || !isset($_SESSION["intentos"])) {
     nuevaPartida($secreto,$intentos);
 } else {
-    recuperarDatosPartida($secreto,$intentos,$historico);
+    recuperarDatosPartida($secreto,$intentos,$historico,$numPartidas);
 }
 
 
@@ -44,6 +67,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["numero"])) {
     $historico = guardarHistorico($numero);
     if ($intentos >= 5) {
         $mensaje = "Ha superado el número de intentos, el número era: ".$secreto;    
+        ActualizarPartidasJugadas($numPartidas);
         nuevaPartida($secreto,$intentos,$historico);
     } else {
         //Menor
@@ -58,6 +82,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["numero"])) {
         if($numero  == $secreto) {
             $mensaje = "Has acertado el número era: ".$numero ;
             nuevaPartida($secreto,$intentos,$historico);
+            ActualizarPartidasJugadas($numPartidas);
         }
     }
 }
@@ -81,6 +106,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["numero"])) {
         <input type="number" name="numero" min="<?=$minimo?>" max="<?=$maximo?>" id="numero">
         <input type="submit" value="Comprobar">
     </form>
+    <p>Partidas Jugadas: <?=$numPartidas ?></p>
     <?=$secreto?>
 </body>
 </html>
